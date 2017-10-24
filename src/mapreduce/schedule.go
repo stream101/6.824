4cb5bc2939118ede637bee87bfacd76905982ec6
xinxin <xinxin@cs.ucsd.edu>
2017-10-23 17:16:11 -0700

    lab1, part3

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+    "fmt"
+    "sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -11,6 +14,20 @@ import "fmt"
 // suitable for passing to call(). registerChan will yield all
 // existing registered workers (if any) and new ones as they register.
 //
+func execTask(addr string, task_args DoTaskArgs, registerChan chan string,
+                wg *sync.WaitGroup) {
+    defer wg.Done()
+    call(addr, "Worker.DoTask", task_args, nil)
+    //here need to use non-blocking sending. When all the task is finished,
+    //schedule() will not receive from channel.
+    select {
+        case registerChan <- addr:
+            return
+        default:
+            return
+    }
+}
+
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
@@ -30,7 +47,14 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	// Remember that workers may fail, and that any given worker may finish
 	// multiple tasks.
 	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	//
+    var wg sync.WaitGroup
+    for i:=0; i<ntasks; i++ {
+        addr := <-registerChan
+        task_args := DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
+        wg.Add(1)
+       // go call(addr, "Worker.DoTask", task_args, nil)
+        go execTask(addr, task_args, registerChan, &wg)
+    }
+    wg.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
